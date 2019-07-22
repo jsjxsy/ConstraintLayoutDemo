6adb0764e3b17786286949d3f6e2bd6b0f291e83
xiaosy <xiaosy@weipaitang.com>
2019-07-22 19:52:24 +0800

    添加测试页面

---
+++ app/src/main/java/org/wangchenlong/constraintlayoutdemo/TestActivity.java
@@ -0,0 +1,13 @@
+package org.wangchenlong.constraintlayoutdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
